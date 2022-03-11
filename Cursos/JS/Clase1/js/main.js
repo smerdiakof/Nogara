815892568832402945f0d4732a853351a68a252a
smerdiakof <nikepalacios5@hotmail.com>
2022-03-10 21:10:27 -0500

    clase2

---
+++ Cursos/JS/Clase1/js/main.js
@@ -0,0 +1,139 @@
+var edad = 24;
+
+var nombre;
+
+nombre = 90;
+
+var fondos = 100;
+
+fondos = 150;
+
+fondos = 120;
+
+fondos = -100;
+
+
+var nombre = 'Smerdiakof';
+
+var whats = nombre;
+var face = nombre;
+var ig = nombre;
+var tw = nombre;
+
+/* console.log(nombre);
+console.log(whats);
+console.log(face);
+console.log(ig);
+console.log(tw); */
+
+
+/* var casado = true;
+var muerto = false;
+
+if (casado) {
+    console.log('Hasta que la muerte los separe');
+    
+} */
+
+/* var feliz = true;
+var triste = false;
+
+if (feliz) {
+    console.log('Eres feliz o solo sonries');
+}
+ */
+/* prompt('Cual es tu nombre');
+console.log(prompt('Cual es tu nombre');); */
+
+/* var name = prompt('Cual es tu nombre');
+console.log(name); */
+
+/* confirm('Se comprara la Mac?');
+if ('Se comprara la Mac?') {
+    console.log(true);
+    console.log(false); */
+
+ /*    var casado = confirm('Estas casado');
+
+    if (casado) {
+        console.log('Estas casado');
+    }
+
+    if (casado == false){
+        console.log('No esta casado');
+    } */
+
+
+    
+/* 
+    int num5 =  98.154;
+    float num15 = 1564564.1454564;
+    String name2 = 121564;
+    Boolean casado2 = asdfasdf;
+    JavaScript no tiene tipado, puedes guardar los que tu quieras, JS lo guarda sin importar nada 
+ */
+
+    var num1 = 98;
+    var num2 =  87;
+    var num3 = - 100;
+
+
+    var suma = num1 + num2;
+    var resta = num1 - num2;
+    var division = num1 / num2;
+    var multi = num1 * num2;
+    var modulo = num1 % num2;
+
+/* 
+  console.log(modulo);
+ */
+  
+  var dinero = 1000;
+  var pcGamer = 800;
+
+
+ /*  >mayor que 
+  < menor que
+  == igual que */
+
+/* if (dinero > pcGamer){
+    console.log('Te puedes comprar la PC');
+}
+19
+if (dinero < pcGamer){
+    console.log('No me alcanza para la Pc');
+}
+
+if (dinero == pcGamer) {
+    console.log('e puedes comprar la PC x2');
+
+} */
+
+/* crear un algoritmo:
+ * te va a preguntar que edad tienes
+ * si eres menor no te deja entrar al bar
+ * si eres mayor si te deja entrar
+*/
+
+var mayoriaEdad = prompt('¿Que edad tienes?')
+
+
+if (mayoriaEdad < 18){
+    console.log('No puedes acceder al Bar');
+}
+if (mayoriaEdad > 18) {
+    console.log('Puedes entrar al bar');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cursos/JS/Clase2/js/main.js
@@ -0,0 +1,28 @@
+/* operadores de comparacion */
+
+/* !=   "diferente de"  */
+/*  >= mayor o igual */
+
+
+
+
+/* console.log(num1 == num2); */
+/* 
+if ( edadCliente > minEdad - 1) {
+console.log('num1 es mayor que num2');
+} */
+
+
+/* if ( edadCliente >= minEdad - 1) {
+    console.log('num1 es mayor que num2');
+    } */
+
+
+    var num1 = 1;
+    var num2 = true;
+
+    console.log(num1 == num2);
+
+  /*   if (edadCliente <= maxEdad){
+        console.log('hehehe');
+    } */
